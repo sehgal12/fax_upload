f69e3b9051461059aec117a7b97cf815e9a3f968
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2019-08-21 03:16:59 +0530

    add method to store fax file with metadata in GridFS
    - The method allows for adding file internally to a GridFS setup defined
    - Only global input required is the connection string to mongoDB
    - Adds all associated information in metadata field of fs.files

--- server/server.js
+++ server/server.js
@@ -48,36 +48,76 @@ app.use('/process_fax', function(req, res, next) {
       });
     }
     request.get('http://50.200.140.121:33935/fax', function(err, result) {
+
       if (err) throw err;
-      var objs = JSON.parse(result.body);
-      for (let i = 0; i < objs.length; i++) {
-        var fileName = objs[i].DocumentParams.Hash;
-        var faxFileName;
-        if (objs[i].DocumentParams.Type === 'image/tiff') {
-            faxFileName = fileName + '.tiff';
-        } else if (objs[i].DocumentParams.Type === 'application/pdf') {
-            faxFileName = fileName + '.pdf';
+
+       // setting up gridFS with mongoDB
+      var mongoose = require('mongoose');
+      var Grid = require('gridfs-stream');
+
+
+      const DBURI = 'mongodb://127.0.0.1:27017/faxUploads';
+
+      mongoose.connect(DBURI, {useMongoClient: true, useNewUrlParser: true});
+
+      var connection = mongoose.connection;
+
+      if (connection != undefined) {
+        console.log(connection.readyState.toString());
+
+        var objs = JSON.parse(result.body);
+        for (let i = 0; i < objs.length; i++) {
+          var fileName = objs[i].DocumentParams.Hash;
+          var faxFileName;
+          if (objs[i].DocumentParams.Type === 'image/tiff') {
+              faxFileName = fileName + '.tiff';
+          } else if (objs[i].DocumentParams.Type === 'application/pdf') {
+              faxFileName = fileName + '.pdf';
+          }
+
+          // parsing binary data from base64 FaxImage response
+          var base64Data = objs[i].FaxImage;
+          var binaryData = new Buffer(base64Data, 'base64').toString('binary');
+          // saving fax image to user's directory for storing through mongoDb
+          fs.writeFile(path.join(saveDir, faxFileName), binaryData, 'binary', function(err) {
+            if (err) console.error(err);
+            else console.log(faxFileName + ' saved to ' + saveDir + '!');
+          });
+
+          // parsing metadata, all except FaxImage
+          delete objs[i].FaxImage;
+          var jsonMetadata = JSON.stringify(objs[i]);
+          var metadataFileName = fileName + '.json';
+          fs.writeFile(path.join(saveDir, metadataFileName), jsonMetadata, 'utf-8', function(err) {
+            if (err) console.error(err);
+            else console.log(metadataFileName + ' saved to ' + saveDir + '!');
+          });
+
+          // saving file to mongoDB
+          Grid.mongo = mongoose.mongo;
+          connection.once('open', () => {
+            console.log('connection opened for uploading');
+            var gridfs = Grid(connection.db);
+            if (gridfs) {
+              var streamwriter = gridfs.createWriteStream({
+                filename: faxFileName,
+                mode: 'w',
+                content_type: objs[i].DocumentParams.Type,
+                metadata: objs[i]
+              });
+              fs.createReadStream(path.join(saveDir, faxFileName)).pipe(streamwriter);
+              streamwriter.on('close', function(file) {
+                console.log('file uploaded to mongoDB successfully');
+              });
+            } else {
+              console.error('No GridFS object found!');
+            }
+          });
+
         }
-
-        // parsing binary data from base64 FaxImage response
-        var base64Data = objs[i].FaxImage;
-        var binaryData = new Buffer(base64Data, 'base64').toString('binary');
-        // saving fax image to user's directory for storing through mongoDb
-        fs.writeFile(path.join(saveDir, faxFileName), binaryData, 'binary', function(err) {
-          if (err) console.error(err);
-          else console.log(faxFileName + ' saved to ' + saveDir + '!');
-        });
-
-        // parsing metadata, all except FaxImage
-        delete objs[i].FaxImage;
-        var jsonMetadata = JSON.stringify(objs[i]);
-        var metadataFileName = fileName + '.json';
-        fs.writeFile(path.join(saveDir, metadataFileName), jsonMetadata, 'utf-8', function(err) {
-          if (err) console.error(err);
-          else console.log(metadataFileName + ' saved to ' + saveDir + '!');
-        });
+      } else {
+        console.error("Unable to connect to Database!");
       }
-
       //   request.post({ url: url, formData: formData }, function (err, httpResponse, body) {
       //     if (err) {
       //       return console.error('upload failed:', err);
